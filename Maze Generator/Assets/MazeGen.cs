fc99d83ba83876aee23c71e62688c6b499c94a73
TimmPure <T.j.m.voskuil@gmail.com>
2018-08-08 21:31:08 +0200

    Spawn a grid of cells

---
+++ Maze Generator/Assets/MazeGen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGen : MonoBehaviour {
+
+    public GameObject cellPrefab;
+    public List<GameObject> stack;
+    public Transform cellParent;
+
+    private int height = 20;
+    private int width = 20;
+
+	// Use this for initialization
+	void Start () {
+        stack = new List<GameObject>();
+        SpawnGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnGrid()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject obj = Instantiate(cellPrefab, new Vector3(j, i, 1f), Quaternion.identity, cellParent);
+                stack.Add(obj);
+            }
+        }
+    }
+}
