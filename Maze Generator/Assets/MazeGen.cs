a258935c735a90986b0ea0a991929b48cb22f5a9
TimmPure <t.j.m.voskuil@gmail.com>
2019-09-12 21:15:15 +0200

    Minor refactoring; made instruction text

--- Maze Generator/Assets/MazeGen.cs
+++ Maze Generator/Assets/MazeGen.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MazeGen : MonoBehaviour
 {
@@ -10,13 +11,16 @@ public class MazeGen : MonoBehaviour
     public Transform cellParent;
     public List<Cell> neighbours = new List<Cell>();
     public Cell currentCell;
+    public Cell startingCell;
     public GameObject start;
     public GameObject finish;
+    public TextMeshProUGUI instructionText;
     public bool BeAMazin = false;
+
     private float coolDown = .01f;
 
-    private int height = 30;
-    private int width = 30;
+    private int gridRows = 30;
+    private int gridCols = 30;
     private float timeStamp;
 
     void Start()
@@ -24,11 +28,26 @@ public class MazeGen : MonoBehaviour
         stack = new List<Cell>();
         SpawnGrid();
 
-        currentCell = grid[0];
+        startingCell = grid[0];
+        currentCell = startingCell;
         currentCell.IsVisited(true);
         currentCell.IsCurrent(true);
 
         timeStamp = Time.time + coolDown;
+    }
+
+    void SpawnGrid()
+    {
+        for (int i = 0; i < gridCols; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                Cell obj = Instantiate(cellPrefab, new Vector3(j, i, 1f), Quaternion.identity, cellParent).GetComponent<Cell>();
+                obj.x = j;
+                obj.y = i;
+                grid.Add(obj);
+            }
+        }
     }
 
     void CheckNeighbours()
@@ -59,34 +78,33 @@ public class MazeGen : MonoBehaviour
 
     Cell GetNeighbour(int i, int j)
     {
-        if (i < 0 || j < 0 || i >= width || j >= height)
+        if (i < 0 || j < 0 || i >= gridCols || j >= gridRows)
         {
+            //Cell to check is out of bounds
             return currentCell;
         } else
         {
-            return grid[i + j * width];
+            /* Translating the 1D array to 2D array (of the Cartesian coordinates) 
+             * f.e., in a grid of 4 by 3, numbering the cells:
+             * 0 , 1 , 2 , 3
+             * 4 , 5 , 6 , 7
+             * 8 , 9 , 10, 12
+             * the first cell of the second row, is the first cell of the first row + 4;
+             * the first cell of the third row, is the first cell of the first row + 8;
+             * 
+             * abstracting, we can deduce that (when the array is ordered across rows): 
+             * index = x + ( y * cols )
+             */
+
+             return grid[i + j * gridCols];
         }
     }
-
-    void SpawnGrid()
-    {
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                Cell obj = Instantiate(cellPrefab, new Vector3(j, i, 1f), Quaternion.identity, cellParent).GetComponent<Cell>();
-                obj.x = j;
-                obj.y = i;
-                grid.Add(obj);
-            }
-        }
-    }
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             BeAMazin = true;
+            instructionText.gameObject.SetActive(false);
         }
 
         if (BeAMazin)
@@ -124,29 +142,35 @@ public class MazeGen : MonoBehaviour
             BeAMazin = false;
             start.SetActive(true);
             finish.SetActive(true);
+            //grid[0]?
             GetNeighbour(0, 0).leftWall.enabled = false;
-            GetNeighbour(width - 1, height - 1).rightWall.enabled = false;
+            //grid[cols * rows - 1]?
+            GetNeighbour(gridCols - 1, gridRows - 1).rightWall.enabled = false;
         }
     }
 
-    private void RemoveWalls(Cell c, Cell n)
+    private void RemoveWalls(Cell current, Cell next)
     {
-        if (n.x > c.x)
+        if (next.x > current.x)
         {
-            c.rightWall.enabled = false;
-            n.leftWall.enabled = false;
-        } else if (n.x < c.x)
+            //The next cell we're travelling to is to the right of the current
+            current.rightWall.enabled = false;
+            next.leftWall.enabled = false;
+        } else if (next.x < current.x)
         {
-            c.leftWall.enabled = false;
-            n.rightWall.enabled = false;
-        } else if (n.y > c.y)
+            //Left
+            current.leftWall.enabled = false;
+            next.rightWall.enabled = false;
+        } else if (next.y > current.y)
         {
-            c.topWall.enabled = false;
-            n.bottomWall.enabled = false;
-        } else if (n.y < c.y)
+            //Top
+            current.topWall.enabled = false;
+            next.bottomWall.enabled = false;
+        } else if (next.y < current.y)
         {
-            c.bottomWall.enabled = false;
-            n.topWall.enabled = false;
+            //Bottom
+            current.bottomWall.enabled = false;
+            next.topWall.enabled = false;
         }
     }
 }
